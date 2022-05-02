36a243786b81d069717f280d69717c8b1af621d5
pakwai <virtue.sign@gmail.com>
2022-05-02 11:41:43 +0800

    Configure contract to compile hedera-strato-hashpack.js

--- contract/experiment-strato.js
+++ contract/experiment-strato.js
@@ -1,6 +1,7 @@
 // console.clear();
 require("dotenv").config();
-const {ApiSession, Contract} = require("@buidlerlabs/hedera-strato-js");
+const { ApiSession, Contract } = require("@buidlerlabs/hedera-strato-js");
+const { Hbar, TransactionId } = require("@hashgraph/sdk");
 
 async function CreateNewContract() {
   const { session } = await ApiSession.default();
@@ -11,20 +12,20 @@ async function CreateNewContract() {
   );
 
   const vaultContract = await Contract.newFrom({ path: './TwitterVault.sol' });
-  const liveContract = await session.upload(vaultContract, {_file: { fileMemo: "Hello Strato" }});
+  const liveContract = await session.upload(vaultContract, { _file: { fileMemo: "Hello Strato" } });
   console.log(`Contract Create ContractId: ${liveContract.id} (Solidity ${liveContract.id.toSolidityAddress()})`);
 
   return { session, liveContract };
 }
 
-async function ConnectToContract(contractId = "0.0.34362599") {
+async function ConnectToContract(contractId = "0.0.34376129") {
   const { session } = await ApiSession.default();
   const receiptsSubscription = session.subscribeToReceiptsWith(
     ({ transaction, receipt }) => {
       console.log(`${receipt.status} ${transaction.constructor.name}: ${transaction.transactionId} ${transaction.transactionMemo}`);
     }
   );
-  
+
   const vaultContract = await Contract.newFrom({ path: './TwitterVault.sol' });
   const liveContract = await session.getLiveContract({ id: contractId, abi: vaultContract.interface });
   return { session, liveContract };
@@ -33,6 +34,18 @@ async function ConnectToContract(contractId = "0.0.34362599") {
 async function main() {
   const { liveContract } = await ConnectToContract();
   console.log(Object.keys(liveContract.interface.functions));
+
+  console.log(await liveContract.checkBalance());
+  console.log(await liveContract.vaultLength("elon"));
+
+  // const metaArgs = {
+  //   amount: new Hbar(1),
+  //   maxTransactionFee: new Hbar(0.1),
+  //   transactionId: TransactionId.generate(liveContract.session.wallet.account.id),
+  //   transactionMemo: "Custom memo",
+  //   transactionValidDuration: 69,
+  // };
+  // console.log(await liveContract.transferFund(metaArgs, "elon"));
 }
 
 main();
