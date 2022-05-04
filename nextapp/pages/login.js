e96af4bb43c64c28cf5271e8b0bdb22c22387df2
pakwai <virtue.sign@gmail.com>
2022-05-04 16:20:23 +0800

    login error

--- nextapp/pages/login.js
+++ nextapp/pages/login.js
@@ -1,15 +1,18 @@
 import Head from 'next/head'
-import { useRouter } from 'next/router';
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 import styles from '../styles/Signin.module.css'
 
 export default function Signin() {
   const router = useRouter();
+  const [errorMsg, setErrorMsg] = useState('');
 
   const onClick = async () => {
     try {
       await window.connectToHashPack();
       router.push('/');
     } catch (error) {
+      setErrorMsg(error);
       console.error(error);
     }
   }
@@ -26,6 +29,9 @@ export default function Signin() {
           <h1 className="mb-1 fw-normal">TwittBar</h1>
           <p className="mb-3">Transfer to Hbar(ℏ) vault via Twitter@handle</p>
           <div className="w-100 btn btn-lg btn-primary" onClick={onClick}>Connect Wallet</div>
+          {
+            errorMsg && <div className='text-danger mt-2'>{errorMsg}</div>
+          }
           <img className="mt-5" src="/built-on-hedera.png" alt="" width="120"></img>
           <p className="mb-3 text-muted">choong.pw &copy; 2022</p>
         </form>
