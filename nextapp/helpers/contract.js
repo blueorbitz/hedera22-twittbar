a7d22285f96630a3d1c6fd3e14a0d7355729fb18
pakwai <virtue.sign@gmail.com>
2022-05-11 19:37:54 +0800

    Experiment global call to test timeout on Vercel.

--- nextapp/helpers/contract.js
+++ nextapp/helpers/contract.js
@@ -1,7 +1,9 @@
 import { ApiSession, Contract } from "@buidlerlabs/hedera-strato-js";
 
 export default async function ConnectToContract(contractId = process.env.CONTRACT_ID) {
+  console.log('Contract call: ConnectToContract()');
   const { session } = await ApiSession.default();
+  console.log('Contract call: connecting interface...');
   const vaultContract = await Contract.newFrom({ path: './TwitterVaultAPI.sol' });
   const liveContract = await session.getLiveContract({ id: contractId, abi: vaultContract.interface });
   return { session, liveContract };
